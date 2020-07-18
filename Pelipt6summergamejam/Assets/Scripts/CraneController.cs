040063921adc1f08be4fb25436322ff64c5a1376
Joonas Einola <joonas.einola@gmail.com>
2020-07-18 13:49:30 +0300

    Update CraneController.cs
    Päivitetty CraneController
    - Lisätty ThisIsTheWind() funktio emuloimaan tuulta
    - Lisätty Inspektorissa näkyvät muuttujat:
       - windForce (tuulen nopeus)
      - windBlastMinWaitForSec (minimiaika tuulenpuuskien välilllä)
      - windBlastMaxWaitForSec (maksimiaika tuulenpuuskien välillä)
      - containerVelocityDamper (vähentää tippuvan kontin perimää velocityä)

--- Pelipt6summergamejam/Assets/Scripts/CraneController.cs
+++ Pelipt6summergamejam/Assets/Scripts/CraneController.cs
@@ -4,33 +4,63 @@ using UnityEngine;
 
 public class CraneController : MonoBehaviour
 {
-    public Rigidbody2D rb;
+    public Rigidbody2D craneLoadRB; // Connect to Crane Load Rigidbody. Windforce affects this Rigidbody.
 
+
+
+    [SerializeField]
+    private int windForce = 1;
+
+    [SerializeField]
+    private int windBlastMinWaitForSec = 1;
+
+    [SerializeField]
+    private int windBlastMaxWaitForSec = 3;
+
+    private bool lastWindBlastDirectionWasRight = false;
+
+    [SerializeField]
+    private int containerVelocityDamper = 1; // This is used to decide how much velocity the falling container inherits from the swinging container.
+
+    private float horizontalMovement; // Currently used to simulate windForce.
+    private float verticalMovement;
+
+    private bool canLoadNewContainer; // This is used to decide when player can drop the next container and when to show container hanging from the crane.
+
+    // Line Renderer uses these two GameObjects to draw the crane cable.
     public GameObject craneTop;
     public GameObject craneLoad;
 
-    public GameObject newContainer;
+    public GameObject newContainer; // Container prefab goes here.
 
-    public LineRenderer lineRenderer;
+    public LineRenderer lineRenderer; // This draws the crane cable.
 
-    private bool canLoadNewContainer;
 
-    private float horizontalMovement;
+
+    Vector3 craneLoadPrevLoc = Vector3.zero;
+    Vector3 craneLoadCurrLoc = Vector3.zero;
+
 
 
     void Start()
     {
         canLoadNewContainer = true;
+        StartCoroutine(ThisIsTheWind());
+
+        craneLoadPrevLoc = craneLoad.transform.position;
     }
 
     void Update()
     {
         horizontalMovement = Input.GetAxisRaw("Horizontal");
+        verticalMovement = Input.GetAxisRaw("Vertical");
 
         if (Input.GetKeyDown(KeyCode.Space) && canLoadNewContainer)
         {
             StartCoroutine(NewContainer());
         }
+
+
     }
 
     void LateUpdate()
@@ -42,7 +72,32 @@ public class CraneController : MonoBehaviour
 
     void FixedUpdate()
     {
-        rb.AddForce(transform.right * horizontalMovement * 10);
+        craneLoadRB.AddForce(transform.right * horizontalMovement * 10);
+    }
+
+    IEnumerator ThisIsTheWind()
+    {
+        craneLoadCurrLoc = (craneLoad.transform.position - craneLoadPrevLoc) / Time.deltaTime; // Crane swing direction. Positive right, negative left.
+
+        // Randomizing the Wind Force for more realistic wind effect
+        int windForceMin = windForce;
+        int windForceMax = windForce * 5;
+        int windFactor = Random.Range(windForceMin, windForceMax);
+
+        if (craneLoadCurrLoc.x > 0)  // Wind blows from right
+        {
+            craneLoadRB.AddForce(transform.right * windFactor);
+            lastWindBlastDirectionWasRight = true;
+        }
+        else  // Wind blows from left
+        {
+            craneLoadRB.AddForce(transform.right * -windFactor);
+            lastWindBlastDirectionWasRight = false;
+        }
+
+        int windBlastTimer = Random.Range(windBlastMinWaitForSec, windBlastMaxWaitForSec);
+        yield return new WaitForSeconds(windBlastTimer);
+        StartCoroutine(ThisIsTheWind());
     }
 
     IEnumerator NewContainer()
@@ -51,7 +106,7 @@ public class CraneController : MonoBehaviour
         canLoadNewContainer = false;
         craneLoad.GetComponent<SpriteRenderer>().enabled = false;
         container = Instantiate(newContainer, craneLoad.transform.position, craneLoad.transform.rotation);
-        container.GetComponent<Rigidbody2D>().velocity = craneLoad.GetComponent<Rigidbody2D>().velocity / 3;
+        container.GetComponent<Rigidbody2D>().velocity = craneLoad.GetComponent<Rigidbody2D>().velocity / containerVelocityDamper;
         yield return new WaitForSeconds(2);
         craneLoad.GetComponent<SpriteRenderer>().enabled = true;
         canLoadNewContainer = true;
